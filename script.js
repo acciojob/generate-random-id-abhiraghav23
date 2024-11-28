c6f5bebafcf9e11cf0fca2f8ddcc1bf9e0ee60c0
Abhishek Raghav <115450330+abhiraghav23@users.noreply.github.com>
2024-11-28 12:20:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function makeid(l) {
-   const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let result = "";
   for (let i = 0; i < l; i++) {
-    const randomIndex = Math.floor(Math.random() * characters.l);
+    const randomIndex = Math.floor(Math.random() * characters.length);
     result += characters[randomIndex];
   }
   return result;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
-alert(makeid(l));
+const l = parseInt(prompt("Enter a number. "), 10);
+alert(makeid(l)); 
